340e3044c88edb5c5b94c282c94653f77fe1f767
Brandon Figueredo <figueredo407@gmail.com>
2021-11-02 12:21:09 -0400

    added handlers for login and signup

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
+import * as fs from 'fs';
+
+const filepath = "saveState.txt";
+let file = {};
+
 const express = require("express");
 
 const app = express();
 
 app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+function reload(filename) {
+    if (fs.existsSync(filename)) {
+        let someStr = fs.readFileSync(filename);
+        counter = JSON.parse(someStr);
+    }
+    else {
+        counter = {};
+    }
+}
+
 
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/home.html");
@@ -22,6 +40,25 @@ app.get("/account", function(req, res) {
 
 app.get("/clubnews", function(req, res) {
     res.sendFile(__dirname + "/clubs_news.html");
+});
+
+app.post("/createuser", function(req, res) {
+    res.send('request to signup received');
+    console.log(req.body);
+
+    // extract information here
+
+    // reload page
+});
+
+app.post("/loginuser", function(req, res) {
+    res.send('request to login received');
+    console.log(req.body);
+
+    // extract information here
+
+    // reload page
+
 });
 
 app.listen(8080, function() {
