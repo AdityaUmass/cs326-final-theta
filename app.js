5cc83ac56d5ea8440ab4d37727d777e8a037b030
Brandredo <figueredo407@gmail.com>
2021-11-03 18:54:15 -0400

    functions for creating a user and accessing a user

--- app.js
+++ app.js
@@ -1,26 +1,63 @@
 const fs = require("fs");
 const express = require("express");
-
-const filepath = "saveState.txt";
-let file = {};
-
-const username = "";
-const loggedin = false;
-
 const app = express();
 
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-function reload(filename) {
+
+const userfile = "userFile.json";
+let trackUsers = { users: [] };
+
+const postsFile = "postFile.json";
+let trackPosts = { posts: [] };
+
+let username = "";
+let loggedin = false;
+
+// function reloads the file (for temporary persistant storage)
+function reload(filename, kind) {
+    
     if (fs.existsSync(filename)) {
         let someStr = fs.readFileSync(filename);
-        counter = JSON.parse(someStr);
+        
+        if(kind === "users") {
+            trackUsers = JSON.parse(someStr);
+        } else {
+            // file for posts
+        }
+    } else {
+        
     }
-    else {
-        counter = {};
+}
+
+// creates a user
+function createUser(req, res) {
+    
+    reload(userfile, "users");
+
+    trackUsers.users.push(req.body);
+    let str = JSON.stringify(trackUsers);
+    fs.writeFileSync(userfile, str);
+    
+    // console.log(trackUsers.users);
+}
+
+// 
+function readUser(req, res) {
+
+    reload(userfile, "users");
+    
+    // what to do with this info? Send to client, print, etc...
+    let user = trackUsers.users.find(x => x === req.body.accountemail);
+
+    if(user === undefined) {
+        res.send("User not found in the system");
+    } else {
+        res.send("user found");
     }
+
 }
 
 
@@ -56,22 +93,31 @@ app.get("/clubnews", function(req, res) {
 });
 
 app.post("/createuser", function(req, res) {
-    res.send('request to signup received');
+    
     console.log(req.body);
 
     // extract information here
+    username = req.body.accountemail;
+    loggedin = true;
+
+
+    createUser(req, res);
 
     // reload page
+    res.redirect('/');
 });
 
 app.post("/loginuser", function(req, res) {
-    res.send('request to login received');
+    
     console.log(req.body);
 
     // extract information here
+    username = req.body.accountemail;
+    loggedin = true;
 
-    // reload page
-
+    // find user info
+    readUser(req, res);
+    res.redirect('/');
 });
 
 app.post("/createPost", function(req, res) {
@@ -82,6 +128,8 @@ app.post("/createPost", function(req, res) {
     //unique-id
     res.redirect("/");
 });
+
+
 
 app.listen(8080, function() {
     console.log("Server started on port 8080");
