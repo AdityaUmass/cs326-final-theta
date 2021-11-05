f215271b764da199772f21afffe0701930cdaf70
Raghav <raghavm@outlook.com>
2021-11-04 22:36:53 -0400

    card creation

--- createCard.js
+++ createCard.js
@@ -24,9 +24,53 @@ function renderAccountPage(element, data){
         divHeader.appendChild(span3);
         divHeader.appendChild(span4);
 
-        element.appendChild(divHeader);
+        divCard.appendChild(divHeader);
 
         //BUILD BODY
-        const 
+        const divBody = document.createElement("div");
+        divBody.classList.add("card-body");
+        
+        const divTitle = document.createElement("h4");
+        divTitle.classList.add("card-title");
+        divTitle.innerText = post["title"];
+        divBody.appendChild(divTitle);
+
+        const bodyText = document.createElement("p");
+        bodyText.classList.add("card-text");
+        bodyText.innerText = post["content"];
+        divBody.appendChild(bodyText);
+
+        const interestedSpan = document.createElement("span")
+        interestedSpan.classList.add("text-muted");
+        interestedSpan.innerText = post["liked_count"] + " interested";
+        divBody.appendChild(interestedSpan);
+
+        divCard.appendChild(divBody);
+
+        //BUILD FOOTER
+        const divFooter = document.createElement("div");
+        divFooter.classList.add("card-footer");
+
+        const buttonEmail = document.createElement("button");
+        buttonEmail.classList.add("btn", "btn-spl");
+        buttonEmail.setAttribute("data-bs-toggle", "modal");
+        buttonEmail.setAttribute("data-bs-target", "modal" + post[_id]);
+        buttonEmail.innerText = "See all interested";
+        divFooter.appendChild(buttonEmail);
+
+        const buttonUpdate = document.createElement("button");
+        buttonUpdate.classList.add("btn", "btn-primary", "flr");
+        buttonUpdate.innerText = "Update"
+        divFooter.appendChild(buttonUpdate);
+
+        const buttonDelete = document.createElement("button");
+        buttonDelete.classList.add("btn", "btn-danger", "flr");
+        buttonDelete.innerText = "Delete"
+        divFooter.appendChild(buttonDelete);
+
+        divCard.appendChild(divFooter);
+
+        //Add card to list
+        element.appendChild(divCard);
     });
 }
