b5fa473e19db7b6b7336f36977731c97fea3ddd2
Brandon Figueredo <figueredo407@gmail.com>
2021-11-17 09:34:30 -0500

    added files

--- app.js
+++ app.js
@@ -1,22 +1,97 @@
 /* jshint esversion: 6*/
 const fs = require("fs");
-const express = require("express");
 const { RSA_NO_PADDING } = require("constants");
+const express = require('express');
+
+// user authentication modules
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const bcryptjs = require("bcryptjs");
+
+// database modules
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// connect the database
+const connect = require("./database");
+
+// database schemas
+const User = require("./models/user");
+const Post = require("./models/post");
+
 //const homeJS = require("./public/js/home.js"); 
+let username = '';
+let loggedin = false;
+let filtered = false;
+
 
 const app = express();
-
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true
+}));
 
-let username = "";
-let loggedin = false;
-let filtered = false;
-let update
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+  
+passport.deserializeUser(async function(id, done) {
+    User.findById(id, function(err, user) {
+        done(err, user);
+    });
+});
+
+// use passport local strategy
+passport.use(
+    new LocalStrategy((username, password, done) => {
+      User.findOne({ username: username }, (err, user) => {
+        if (err) { 
+          return done(err);
+        }
+
+        if (!user) {
+          return done(null, false, { message: "Incorrect username" });
+        }
+        
+        bcryptjs.compare(password, user.password, (err, res) => {
+            
+            if(res) {
+                console.log("passwords match");
+                return done(null, user);
+            } else if(err) {
+                console.log(err);
+                return done(err);
+            } else {
+                console.log("passwords dont match");
+                return done(null, false);
+            }
+        });
+      });
+    })
+);
+
+// keep these here *IMPORTANT*
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+// checks whether a user is logged in
+function isLoggedIn(req, res, next) {
+    if(req.user) { // user is not logged in
+        next();
+    } else {
+        res.redirect('/'); // user is not logged in
+    }
+}
 
 // endpoint for homepage
 app.get("/", function(req, res) {
+    
     //render functions
     if (!filtered) {
         let posts = JSON.parse(fs.readFileSync("posts.json"));
@@ -31,6 +106,42 @@ app.get("/", function(req, res) {
     res.sendFile(__dirname + "/home.html");
 });
 
+// enpoint for creating a user
+app.post('/createUser', function(req, res) {
+    
+    bcryptjs.hash(req.body.password, 10, (err, hashedPassword) => {
+        // handle error
+        if(err) {
+            console.log("couldn't hash password");
+        }
+        // store user into the database
+        const user = new User({
+            name: req.body.name,
+            username: req.body.username,
+            password: hashedPassword,
+            posts: []
+        }).save().then(entry => {
+            console.log(entry);
+        }).catch(err => {
+            console.log(err);
+        });
+
+    });
+
+    res.redirect("/");
+});
+
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/"
+}));
+
+app.get("/signout", (req, res) => {
+    req.logout();
+    res.redirect("/");
+});
+
+
 // endpoint will get the posts page
 app.get("/post", function(req, res) {
     res.sendFile(__dirname + "/post.html");
@@ -39,7 +150,7 @@ app.get("/post", function(req, res) {
 // endpoint will send the navigation bar based on login status
 app.get("/navbar", function(req, res) {
     
-    if(loggedin) {
+    if(req.user) {
         res.sendFile(__dirname + "/navbarlog.html");
     } else {
         res.sendFile(__dirname + "/navbar.html");
@@ -52,27 +163,27 @@ app.get("/account", function(req, res) {
     //pull data from the database using global username
     //render that data on to account.html
 
-    if(!loggedin) {
-        res.status(400).send("User not logged in");
-        return;
-    }
+    // if(!loggedin) {
+    //     res.status(400).send("User not logged in");
+    //     return;
+    // }
 
-    if (fs.existsSync("posts.json")){
-        let posts = JSON.parse(fs.readFileSync("posts.json"));
-        let i = {};
-        loggedInPosts = [];
-        posts.forEach(i => {
-            console.log(i);
-            if (i["author"] === username){
-                loggedInPosts.push(i);
-            }
-        });
-        console.log(loggedInPosts);
-        fs.writeFileSync("myAccount.json", JSON.stringify(loggedInPosts));
-    }
-    else {
-        fs.writeFileSync("myAccount.json", "[]");
-    }
+    // if (fs.existsSync("posts.json")){
+    //     let posts = JSON.parse(fs.readFileSync("posts.json"));
+    //     let i = {};
+    //     loggedInPosts = [];
+    //     posts.forEach(i => {
+    //         console.log(i);
+    //         if (i["author"] === username){
+    //             loggedInPosts.push(i);
+    //         }
+    //     });
+    //     console.log(loggedInPosts);
+    //     fs.writeFileSync("myAccount.json", JSON.stringify(loggedInPosts));
+    // }
+    // else {
+    //     fs.writeFileSync("myAccount.json", "[]");
+    // }
     res.sendFile(__dirname + "/account.html");
 });
 
@@ -149,252 +260,82 @@ app.get("/clubnews", function(req, res) {
     res.sendFile(__dirname + "/clubs_news.html");
 });
 
-// endpoint for creating a new user
-app.post("/createuser", function(req, res) {
-    
-    // opens a file
-    let trackUsers = { users: [] };
-    if(fs.existsSync("users.json")) {
-        trackUsers = JSON.parse(fs.readFileSync("users.json"));
-    }
-    
-    // finds user
-    let userExists = trackUsers.users.some(user => user.accountemail === req.body.accountemail);
-    
-    // sends an error if the user already exists
-    if(userExists) {
-        res.status(400).send('An account with that email already exists');
-        return;
-    }
-    
-    // extract information here
-    username = req.body.accountemail;
-    loggedin = true;
-
-    // push the user for the file
-    trackUsers.users.push(req.body);
-    let str = JSON.stringify(trackUsers);
-    fs.writeFileSync("users.json", str);
-
-    // reload page
-    res.redirect('/');
-});
-
-// endpoint for logging in
-app.post("/loginuser", function(req, res) {
- 
-    // open file and find user
-    let trackUsers = JSON.parse(fs.readFileSync('users.json'));
-    let user = trackUsers.users.find(x => x.accountemail === req.body.accountemail);
-
-
-    // checks if the user exists
-    if(user === undefined) {
-        res.status(400).send('Account not found');
-    } else {
-        // then the user was found, but password authentication is still needed (next milestone?)
-        // extract information here
-        if(user.accountpassword !== req.body.accountpassword) {
-            res.status(400).send('Incorrect password');
-        }
-    
-        username = req.body.accountemail;
-        loggedin = true;
-        res.redirect('/');
-    }
-});
-
-// endpoint for signing out
-app.get("/signout", function(req, res) {
-    loggedin = false;
-    username = "";
-    res.redirect('/');
-});
-
 // endpoint for going to the update info page
 app.get("/updateInfo", function(req, res) {
     res.sendFile(__dirname + "/accountUpdate.html");
 })
 
 // endpoint for updating user account information
-app.post("/updateAccountInfo", function(req, res) {
+app.post("/updateAccountInfo", isLoggedIn, async function(req, res) {
+    
+    console.log(req.body);
     
     // save old email
-    let oldEmail = username;
+    let oldEmail = req.user.username;
     
-    // open the file and find the user if there is one
-    let trackUsers = JSON.parse(fs.readFileSync('users.json'));
-    let user = trackUsers.users.find(x => x.accountemail === username);
-
-    // check if the email was changed
     if(req.body.useremail.length !== 0) {
-        
-        user.accountemail = req.body.useremail;
-        
-        // change username
-        username = user.accountemail;
-
-        // update the posts file to replace the old email with the new email
-        let posts = JSON.parse(fs.readFileSync("posts.json"));    
-        
-        posts.forEach( post => { 
-        
-            if(post["author"] === oldEmail) {
-                post["author"] = username;
-            }
             
-            post["liked_username"].forEach( (name, index) => {
-                if(name === oldEmail) {
-                    post["liked_username"][index] = username;
-                }        
-            });
-    
+        User.findOneAndUpdate({ username: req.user.username }, {username: req.body.useremail}).catch(err => {
+            console.log(err);
         });
-
-        // write the new file
-        fs.writeFileSync("posts.json", JSON.stringify(posts));
-
-
-        // update the render file to replace the old email with the new email
-        let render = JSON.parse(fs.readFileSync("render.json"));
-
-        if(render["userName"] === oldEmail) {
-            render["userName"] = username;
-        }
-
-        render["posts"].forEach( post => { 
         
-            if(post["author"] === oldEmail) {
-                post["author"] = username;
-            }
-            
-            post["liked_username"].forEach( (name, index) => {
-                if(name === oldEmail) {
-                    post["liked_username"][index] = username;
-                }        
-            });
-    
-        });
-
-        fs.writeFileSync("render.json", JSON.stringify(render));
-    
     }
 
-    // check if the account name was changed
     if(req.body.accountname.length !== 0) {
-        user.accountname = req.body.accountname;
+        User.findOneAndUpdate({ username: req.user.username }, {name: req.body.accountname}).catch(err => {
+            console.log(err);
+        });
     }
 
-    // check if the password was changed
     if(req.body.userpassword.length !== 0) {
-        user.accountpassword = req.body.userpassword;
+
+        //need to hash the password first
+        bcryptjs.hash(req.body.userpassword, 10, (err, hashedPassword) => {
+            // handle error
+            if(err) {
+                console.log("couldn't hash password");
+            }
+            
+            User.findOneAndUpdate({ username: req.user.username }, {password: hashedPassword}).catch(err => {
+                console.log(err);
+            });
+        });
     }
 
-    // write the new users
-    fs.writeFileSync("users.json", JSON.stringify(trackUsers));
-
-    // display that changes were saved
-
-    res.redirect('/updateInfo');
+    // find every instance of the old username in each liked_username array
+    User.updateMany({ liked_username: oldEmail}, {})
+    
+    res.redirect('/account');
 });
 
 
 // endpoint for creating a new post
-app.post("/createPost", function(req, res) {
-    if(!loggedin) {
-        res.status(400).send("User not logged in");
-        return;
-    }
-
-    let posts = JSON.parse(fs.readFileSync("posts.json"));
-
-    let post = {}
-    let formData = req.body;
-
-    post["author"] = username;
-    post["liked_count"] = 0;
-    post["liked_username"] = [];
+app.post("/createPost", isLoggedIn, function(req, res) {
     
-    let newID = 0;
+    console.log(req.user);
+    console.log('entering create a post endpoint');    
     
-    while(true) {
-        let foundID = true;
-        for (let post of posts) {
-            if (post["_id"] === newID) {
-                foundID = false;
-                newID++;
-                break;
-            }
-        }
-
-        if (foundID === true) {
-            break;
-        }
-    }
-    
-    post["_id"] = newID;
-
-    post["title"] = formData.title;
-    post["content"] = formData.content;
-    post["activity"] = formData.activity;
-    post["workout"] = formData.workout;
-    post["duration"] = formData.duration;
-    post["time"] = formData.time;
-    post["contact"] = formData.contact;
-    
-    if(formData["date"].length !== 0) {
-        post["date"] = formData.date;
-        post["days"] = [];
-    } else {
-        let days = [];
-        if ("Monday" in formData) {
-            days.push("Monday");
-        }
-
-        if ("Tuesday" in formData) {
-            days.push("Tuesday");
-        }
-
-        if ("Wednesday" in formData) {
-            days.push("Wednesday");
-        }
-
-        if ("Thursday" in formData) {
-            days.push("Thursday");
-        }
-
-        if ("Friday" in formData) {
-            days.push("Friday");
-        }
-
-        if ("Saturday" in formData) {
-            days.push("Saturday");
-        }
-
-        if ("Sunday" in formData) {
-            days.push("Sunday");
-        }
-
-        post["days"] = days;
-        post["date"] = "";
-    }
-    
-    posts.push(post);
-
-
-
-    fs.writeFile("posts.json", JSON.stringify(posts), (err) => {
-        "Post creation error.";
+    const post = new Post({
+        author: req.user.username,
+        liked_count: 0,
+        liked_username: [],
+        title: req.body.title,
+        content: req.body.content,
+        activity: req.body.activity,
+        workout: req.body.workout,
+        duration: req.body.duration,
+        time: req.body.time,
+        contact: req.body.contact,
+        date: req.body.date,
+        days: req.body.days
+    }).save().then(entry => {
+        console.log(entry);
+    }).catch(err => {
+        console.log(err);
+        console.log('post couldn\'t be saved in database');
     });
-
-    let renderInfo = {"userName": username, "posts": posts};
-
-    fs.writeFile("render.json", JSON.stringify(renderInfo), (err) => {
-        "Write error.";
-    });
-
+    
     res.redirect("/");
-    //homeJS.render(posts, username);
 });
 
 app.post("/filter", function(req, res) {
@@ -526,6 +467,10 @@ app.get("/renderjson", function(req, res) {
     res.sendFile(__dirname + "/render.json");
 });
 
-app.listen(process.env.PORT || 8080, function() {
-    
+app.listen(8080, function(err) {
+    if(err) {
+        console.log("couldn't connect to server");
+    }
 });
+
+
